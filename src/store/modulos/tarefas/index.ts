d7f4ad555593d518c08cb8db6ccca0cb5c1d6060
Bruno Tenório <tenoriobn@gmail.com>
2024-04-14 11:24:47 -0300

    Reagindo a mudanças

--- src/store/modulos/tarefas/index.ts
+++ src/store/modulos/tarefas/index.ts
@@ -26,8 +26,14 @@ export const tarefa: Module<EstadoTarefa, Estado> = {
     }
   },
   actions: {
-    [OBTER_TAREFAS] ({ commit }) {
-      http.get('tarefas')
+    [OBTER_TAREFAS] ({ commit }, filtro: string) {
+      let url = 'tarefas';
+
+      if (filtro) {
+        url += '?descricao=' + filtro
+      }
+
+      http.get(url)
         .then(resposta => commit(DEFINIR_TAREFAS, resposta.data))
     },
     [CADASTRAR_TAREFA] ({ commit }, tarefa: ITarefa) {
